20e15d94f07240bcd4bcac4f883183a873cfe233
john0 <jcampusano@k3d.do>
2023-04-16 09:51:36 -0400

    formatting build.gradle.kts, build.gradle.kts and settings.gradle.kts files and fixing errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,8 @@ plugins {
 
 android {
     namespace = "com.jecs.mytasklist"
-    compileSdkVersion(33)
+    compileSdk = 33
+
 
     defaultConfig {
         applicationId = "com.jecs.mytasklist"
@@ -24,7 +25,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
@@ -40,7 +41,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -55,20 +56,19 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation ("androidx.activity:activity-compose:1.7.0")
-    implementation ("androidx.compose.ui:ui:$1.4.1")
-    implementation ("androidx.compose.ui:ui-tooling-preview:$1.4.1")
+    implementation ("androidx.compose.ui:ui:1.4.1")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.4.1")
     implementation ("androidx.compose.material:material:1.4.1")
     testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$1.4.1")
-    debugImplementation ("androidx.compose.ui:ui-tooling:$1.4.1")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest:$1.4.1")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.4.1")
+    debugImplementation ("androidx.compose.ui:ui-tooling:1.4.1")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.4.1")
 
     // Compose dependencies
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation ("androidx.navigation:navigation-compose:2.5.3")
-    implementation ("androidx.compose.material:material-icons-extended:$1.4.1")
+    implementation ("androidx.compose.material:material-icons-extended:1.4.1")
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // Coroutines
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 buildscript {
+    extra["compose_ui_version"] = "1.4.1"
+}
 
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-
-dependencies{
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
-    classpath("com.android.tools.build:gradle:8.0.0")
-    classpath("org.jetbrains.kotlin.android:application:8.0.0")
-    classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.dagger.hilt.android") version "2.43.2" apply false
 }
